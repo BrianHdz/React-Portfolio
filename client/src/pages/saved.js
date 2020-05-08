b1823f352635cbffb6eb452e8dd2b2692c8ee3b5
BrianHdz <popcorn46290@sbcglobal.net>
2020-05-08 00:28:43 -0700

    Setting up projects page

--- client/src/pages/saved.js
+++ client/src/pages/saved.js
@@ -4,9 +4,9 @@ import "../App.css";
 
 import Navbar from "../components/Navbar";
 import { Col, Row, Container } from "../components/Grid";
-import { Input, TextArea, FormBtn } from "../components/SearchBar";
 import BoxResults from '../components/BoxResults';
-import Jumbotron from '../components/Jumbotron';
+import Footer from "../components/Footer";
+
 
 import API from "../utils/searchApi";
 
@@ -16,12 +16,12 @@ class Saved extends Component {
         getBooks: [],
     }
 
-    componentDidMount(){
+    componentDidMount() {
         API.getBooks()
-        .then(getBooks => this.setState({
-            getBooks: getBooks
-        })).catch(
-            err => console.log(err)
+            .then(getBooks => this.setState({
+                getBooks: getBooks
+            })).catch(
+                err => console.log(err)
             )
     }
 
@@ -32,20 +32,34 @@ class Saved extends Component {
                 <Navbar />
                 <div className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
-                    <h2>View Your Saved Books</h2>
+                    <h2>Projects</h2>
                 </div>
                 <p className="App-intro">
                     You found the secret message! Nothing gets by you.
             </p>
+<br></br>
+                <Container fluid>
 
-                <Row><Col size="md-12 sm-12">
-                    <div className="justify-content-center">
-                        <h3>Saved</h3></div></Col>
-                </Row>
-                <Row><BoxResults books={this.state.getBooks} /></Row>
+                    <Row>
+                        <Col size="md-12 sm-12">
+                            <div className="justify-content-center">
+                                <h3>A few works in progress...</h3>
+                            </div>
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        <Col size="md-3 sm-12"></Col>
+                        <Col size="md-6 sm-12" className="justify-content-center">
+                            <BoxResults books={this.state.getBooks} />
+                        </Col>
+                        <Col size="md-3 sm-12"></Col>
+                    </Row>
 
 
 
+                </Container>
+                <Footer />
             </div>
         );
     }
